8393adb781be2729fac1a8e27a837bf07201610f
naicm12 <163166528+naicm12@users.noreply.github.com>
2024-03-18 10:57:29 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import streamlit as st
+iimport streamlit as st
 import re
 from datetime import date
 
@@ -9,6 +9,8 @@ tela_ativa = 0
 from PIL import Image
 img = Image.open('001.png')
 st.image(img)
+st.markdown("### 3º Desafio 200k - Porto Velho/Humaitá")
+
 st.page_link("http://191.217.246.233:8501/", label="Clique aqui para realizar a inscrição")
 
 
@@ -16,3 +18,4 @@ st.page_link("http://191.217.246.233:8501/", label="Clique aqui para realizar a 
 from PIL import Image
 img = Image.open('003.png')
 st.image(img)
+
