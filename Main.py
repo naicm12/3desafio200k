a14b94bff9278fa598bde875ea3d89375eaf0c2d
naicm12 <163166528+naicm12@users.noreply.github.com>
2024-03-23 18:57:58 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,7 +13,7 @@ with st.form("Informativo"):
     st.write("📍 Saída: Início do Espaço Alternativo - Av. Jorge Teixeira (Praia)")
     st.caption("")
     st.write("💲 Valor da Inscrição: 500,00 reais Solo e 400,00 reais se for em equipe (valor da inscrição é individual)")
-    st.link_button(label="Clique aqui para realizar sua inscrição",url="https://desafio200k-inscricao-1727ec5a1e6a.herokuapp.com/")
+    st.link_button(label="Clique aqui para realizar sua inscrição",url="https://desafio200k-inscricao-1727ec5a1e6a.herokuapp.com/",type="primary")
     #st.link_button(label="Clique aqui para realizar sua inscrição",url="http://191.217.246.233:8501/",type="primary")
     st.write(" Forma de Pagamento: ")
     st.write("  Pix kelioesteves@hotmail.com - Kélio Esteves Xavier - Mercado pago.")
@@ -34,7 +34,7 @@ with st.form("Regulamento"):
         lines = f.readlines()
         for line in lines:
             st.write(line)
-    st.link_button(label="Clique aqui para realizar sua inscrição",url="https://desafio200k-inscricao-1727ec5a1e6a.herokuapp.com/")
+    st.link_button(label="Clique aqui para realizar sua inscrição",url="https://desafio200k-inscricao-1727ec5a1e6a.herokuapp.com/",type="primary")
     #st.link_button(label="Clique aqui para realizar sua inscrição",url="http://191.217.246.233:8501/",type="primary")
     st.form_submit_button("",disabled=True)
 
